65950b867c9a117ed99774a3a55061310e24a649
rajivmathur77 <57667107+rajivmathur77@users.noreply.github.com>
2023-07-18 01:14:55 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 import pandas
-my_Fruit_List= pandas.read_csv(https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
+my_Fruit_List= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_Fruit_List)
